ac36b007b80817d2e1e174403c70c4a90ceb2de0
Sony111 <Sony111@users.noreply.github.com>
2014-06-19 22:52:52 -0700

    Update makeCacheMatrix.R

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,20 +1,21 @@
-## Create a "CacheVector" that will cache and  calculate  its inverse
+## Create a "CacheMatrix" with vector,nrow and ncol as arguments
 makeCacheMatrix <- function(m,r,c) {
         m1 <- NULL
+        ## with setMatrix we are creating matrix 
       setMatrix <- function(r,c) {
 	print(r)
       m <<- matrix(m,nrow=r,ncol=c) 
 	m1 <<- NULL
     }
-    
+    ## with getMatrix we are assigning values
     getMatrix <- function() {
         m
     }
-    
+    ## assign value to setcacheSolve function
     setcacheSolve <- function(inverse) {
         m1 <<- inverse
     }
-    ## create a cacheSolve function that returns the inverse of a matrix,it if necessary/existing simply display
+    ## create a cacheSolve function that returns the inverse of a matrix,(it if necessary )or else simply display existing 
     cacheSolve <- function() {
         if (is.null(m1)) {
             m1 <<- solve(m)
